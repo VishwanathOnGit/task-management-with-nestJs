49e73a54a504ab6322ff91bad7bbd66307a01937
Vishwanath <putchavishwanath@gmail.com>
2025-03-07 17:23:30 +0530

    add filtering functionality to getTasks method in TasksController and implement getTasksWithFilters in TasksService

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,17 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAlltasks(): Task[] {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 
   getTaskById(id: string): Task | undefined {
     return this.tasks.find((task) => task.id === id);
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+
+        return false;
+      });
+    }
+
+    return tasks;
   }
 
   createTask(CreateTaskDto: CreateTaskDto): Task {
